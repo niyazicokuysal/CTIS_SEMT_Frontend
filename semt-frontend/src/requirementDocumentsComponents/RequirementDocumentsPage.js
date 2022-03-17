437e921e4d5de1487bee19b8f00a84d494c7be23
dmrkosterit <demirkosterit@gmail.com>
2022-03-17 23:29:15 +0300

    Requirement Modals
    Added Modals for Requirements Page

--- semt-frontend/src/requirementDocumentsComponents/RequirementDocumentsPage.js
+++ semt-frontend/src/requirementDocumentsComponents/RequirementDocumentsPage.js
@@ -17,11 +17,74 @@ import {
 const RequirementDocumentsPage = () => {
   const [project, setProject] = useState([]);
 
-  const now = 60;
+  const [showDoc, setDoc] = useState(false);
+  const [showGroup, setGroup] = useState(false);
+  const [showReq, setReq] = useState(false);
+
+  const [docName, setName] = useState("");
+  const [docDescription, setDesc] = useState("");
+
+  const [reqComment, setReqComment] = useState("");
+  const [reqDesc, setReqDesc] = useState("");
+  const [reqGrp, setReqGrp] = useState("");
+  const [reqType, setReqType] = useState("");
+
+  const [groupName, setGroupName] = useState("");
 
   const { pathname } = useLocation();
   const path = pathname.split("/");
   const projId = path[1];
+
+  const docClose = () => setDoc(false);
+  const docShow = () => setDoc(true);
+
+  const groupClose = () => setGroup(false);
+  const groupShow = () => setGroup(true);
+
+  const reqClose = () => setReq(false);
+  const reqShow = () => setReq(true);
+
+  const now = 60;
+
+  const onSubmitReq = (e) => {
+    e.preventDefault();
+
+    if (!reqComment || !reqDesc || !docName || !docDescription || !reqGrp || !reqType) {
+      alert("Please add the credentials");
+      return;
+    }
+
+    setReqComment("");
+    setReqDesc("");
+    setReqGrp("");
+    setReqType("");
+    docClose(false);
+  };
+
+  const onSubmitDocument = (e) => {
+    e.preventDefault();
+
+    if (!docName || !docDescription ) {
+      alert("Please add the credentials");
+      return;
+    }
+
+    setName("");
+    setProject("");
+    docClose(false);
+  };
+
+  const onSubmitGroup = (e) => {
+    e.preventDefault();
+
+    if (!groupName) {
+      alert("Please add the credentials");
+      return;
+    }
+
+    setGroup("");
+    docClose(false);
+  };
 
   useEffect(() => {
     const getProject = async () => {
@@ -77,21 +140,23 @@ const RequirementDocumentsPage = () => {
               size="lg"
               variant="success"
               className="btnReqDoc"
-              // onClick={handleShow}
+              onClick={docShow}
+
             >
               Edit Document Info
             </Button>
             <Button
               size="lg"
               variant="danger"
-              className="btnReqDoc" //onClick={docShow}
+              className="btnReqDoc"
+              onClick={reqShow}
             >
               Add Requirement in Document
             </Button>
             <Button
               size="lg"
               variant="info"
-              className="btnReqDoc" //onClick={docShow}
+              className="btnReqDoc" onClick={groupShow}
             >
               Add Requirements Group
             </Button>
@@ -126,7 +191,7 @@ const RequirementDocumentsPage = () => {
                   <td>1</td>
                 </tr>
                 <tr>
-                <td>1</td>
+                  <td>1</td>
                   <td>Mark</td>
                   <td>Otto</td>
                   <td>@mdo</td>
@@ -137,21 +202,21 @@ const RequirementDocumentsPage = () => {
                   <td>1</td>
                 </tr>
                 <tr className="header">
-                  <td colspan="9">ASDFASDF</td>
+                  <td colSpan="9">ASDFASDF</td>
                 </tr>
                 <tr>
-                <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
                   <td>1</td>
                   <td>Mark</td>
                   <td>Otto</td>
                   <td>@mdo</td>
                   <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>1</td>
                 </tr>
                 <tr>
-                <td>1</td>
+                  <td>1</td>
                   <td>Mark</td>
                   <td>Otto</td>
                   <td>@mdo</td>
@@ -166,6 +231,143 @@ const RequirementDocumentsPage = () => {
           </Col>
         </Row>
       </Container>
+
+
+      <Modal
+        //Edit document modal
+        show={showDoc}
+        onHide={docClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Edit Document Info
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={onSubmitDocument}>
+            <Form.Group className="mb-3" controlId="">
+              <Form.Label>Edit Document Name</Form.Label>
+              <Form.Control
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                placeholder="Enter the name for the Document"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="">
+              <Form.Label>Edit Document Description</Form.Label>
+              <Form.Control
+                onChange={(e) => setDesc(e.target.value)}
+                style={{ height: "200px" }}
+                rows="5"
+                as="textarea"
+                placeholder="Enter the description for the Document"
+              />
+            </Form.Group>
+            <Modal.Footer>
+              <Button variant="primary" type="submit">
+                Update Document
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal.Body>
+      </Modal>
+
+      <Modal
+        //Add Requirement In Document
+        show={showReq}
+        onHide={reqClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Add Requirement
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={onSubmitReq}>
+            <Form.Group className="mb-3" controlId="">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                onChange={(e) => setReqDesc(e.target.value)}
+                style={{ height: "100px" }}
+                rows="5"
+                as="textarea"
+                placeholder="Enter the description for the Requirement"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="">
+              <Form.Label>Comment</Form.Label>
+              <Form.Control
+                onChange={(e) => setReqComment(e.target.value)}
+                style={{ height: "100px" }}
+                rows="5"
+                as="textarea"
+                placeholder="Enter the comment for the Requirement"
+              />
+            </Form.Group>
+
+            <Form.Select aria-label="Default select example">
+              <option>Type</option>
+              <option value="1">Inspection</option>
+              <option value="2">Demonstration</option>
+              <option value="3">Test</option>
+              <option value="4">Analysis</option>
+              <option value="5">Certification</option>
+            </Form.Select>
+
+            <Form.Select aria-label="Default select example">
+              <option>Group</option>
+            </Form.Select>
+
+            <Modal.Footer>
+              <Button variant="primary" type="submit">
+                Add Requirement
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal.Body>
+      </Modal>
+
+      <Modal
+        //Add Requirement Group Modal
+        show={showGroup}
+        onHide={groupClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Add Requirement Group
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={onSubmitGroup}>
+            <Form.Group className="mb-3" controlId="">
+              <Form.Label>Group Name</Form.Label>
+              <Form.Control
+                onChange={(e) => setGroupName(e.target.value)}
+                type="text"
+                placeholder="Enter the name for the Group"
+              />
+            </Form.Group>
+
+            <Modal.Footer>
+              <Button variant="primary" type="submit">
+                Add Group
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal.Body>
+      </Modal>
+
     </>
   );
 };
